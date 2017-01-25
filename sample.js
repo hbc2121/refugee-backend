170ad4630289335960270d20fb07e2a8402bd7c1
Hayley Cohen <hbc2121@gmail.com>
2017-01-25 14:16:20 -0500

    pushing this version that prints in plaintext, switching to print in JSON

--- sample.js
+++ sample.js
@@ -4,27 +4,40 @@
 
 var questions = JSON.parse(content);
 
-function print_list_nodropdown(list){
+function print_list(list, dropdown){
 
 	for (var key in list){
 		if(list.hasOwnProperty(key)) {
 			var val = list[key];
+
+			if(!dropdown && key!= "dropdown")
 			console.log(val);
+
+			if(dropdown){
+				for(var key_d in val){
+					if (val.hasOwnProperty(key_d)){
+					var val_d = val[key_d];
+					console.log(val_d);
+					}
+				}
+			}
 		}
 	}
 }
 
-function get_list(category){
-	var list = questions[category];
 
-	if(list.dropdown == false)
-	print_list_nodropdown(list);
+
+function get_list(category){
+
+	var list = questions[category];
+	print_list(list,list.dropdown);
 
 }
 
 //This is what front end peeps do
 var trauma_events = "trauma_events";
 var personal_desc = "personal_desc"
+var head_injury = "head_injury"
 get_list(trauma_events);
 
 

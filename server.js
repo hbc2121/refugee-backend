a33c26339192bc5a4b02f28583ff32d43128cb47
Ethan Donowitz <ethan.donowitz@posteo.org>
2017-01-29 22:29:21 -0500

    added todo comment

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
 
+//TODO: why do we need this
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");

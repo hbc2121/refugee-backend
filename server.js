cd83eb35c0f0d42737be37afc16a52a2db0165e9
Ethan Donowitz <ethan.donowitz@posteo.org>
2017-01-29 22:15:07 -0500

    added code for addGrocery

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
-//var uuid = require('uuid');
- 
+//var bodyParser = require('body-parser');
 var app = express();
 
 // Mongo initialization and connect to database
@@ -11,29 +10,27 @@ var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 });
 
 var error_msg = "{}";
-
-/*
-//enable CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-*/
-
-var config = require('./questions.json');
+var config = require('./question_list.json');
 console.log(config.te1 + ' ' + config.te2);
 
-app.get('/add', function(request,response){
+app.post('/addPatient', function(request,response) {
+	db.collection('patients', function(error, coll) {
+		coll.insert(request.body);	
+	});
 });
 
-app.get('/delete', function(request,response){
+app.get('/deletePatient', function(request,response) {
+	
+});
+
+app.get('/getQuestions', function(request,response) {
+	
 });
 
 app.get('/', function(request, response) {
 	console.log('hi');
 	response.send('hey');
-  });
+});
 
 app.listen(process.env.PORT || 3000);
 

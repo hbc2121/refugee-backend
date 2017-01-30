7e39fcf696dcdee62ea17c451d100d6a573fd2f5
Ethan Donowitz <ethan.donowitz@posteo.org>
2017-01-29 23:06:14 -0500

    code to add stuff to db

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
 // Mongo initialization and connect to database
 //var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL;
-var mongoUri = "mongodb://heroku_5qlth62f:heroku_5qlth62f@ds127389.mlab.com:27389/heroku_5qlth62f";
+var mongoUri = "mongodb://main:password1@ds127389.mlab.com:27389/heroku_5qlth62f";
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
@@ -17,14 +17,15 @@ app.use(function(req, res, next) {
 	next();
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 var error_msg = "{}";
 var config = require('./question_list.json');
 console.log(config.te1 + ' ' + config.te2);
 
 app.post('/addPatient', function(request,response) {
-	db.collection('patients', function(error, coll) {
-		coll.insert(request.body);	
-	});
+	db.collection('patients').insert(request.body);
 });
 
 

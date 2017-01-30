b09a0469d55dd41ae1092a9f9148b9514e94a9bd
Hayley Cohen <hbc2121@gmail.com>
2017-01-29 22:22:44 -0500

    better shit

--- server.js
+++ server.js
@@ -22,11 +22,6 @@ app.post('/addPatient', function(request,response) {
 
 app.get('/get', function(request,response){
 
- var fs = require('fs');
- var content = fs.readFileSync('question_list.json', 'utf8');
- 
- response.setHeader('Content-Type', 'application/json');
- response.send((content));
 
 });
 
@@ -41,6 +36,12 @@ app.get('/deletePatient', function(request,response) {
 });
 
 app.get('/getQuestions', function(request,response) {
+	
+	var fs = require('fs');
+	var content = fs.readFileSync('question_list.json', 'utf8');
+	 
+	response.setHeader('Content-Type', 'application/json');
+	response.send((content));
 	
 });
 

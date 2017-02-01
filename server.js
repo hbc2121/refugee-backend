618a9995b905e2c83f3b8b5c198044007ec014f4
Ethan Donowitz <ethan.donowitz@posteo.org>
2017-01-31 21:55:54 -0500

    readded node modules

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var pdfkit = require('pdfkit');
+var fs = require('fs');
 var app = express();
 
 // Mongo initialization and connect to database
@@ -23,7 +24,6 @@ app.use(bodyParser.urlencoded({ extended: true}));
 
 var error_msg = "{}";
 var config = require('./question_list.json');
-console.log(config.te1 + ' ' + config.te2);
 
 app.post('/addPatient', function(request,response) {
 	db.collection('patients').insert(request.body);
@@ -55,14 +55,31 @@ app.get('/getQuestions', function(request,response) {
 	response.send((content));
 	
 });
+/*
+function printObject(object) {
+	if (object) {
+		for (property in object) {
+			console.log(property + ": " + object[property]);	
+		}
+	}
+}
 
+// TODO: could somebody spam this function and crash the server?
+// 	--> how can we ensure only one PDF request is made at a time?
 app.post('/genPDF', function(request, response) {
-	var email = request.body.email;
-	var obj = request.body.doc;
+	//var email = request.body.email;
+	//var obj = request.body.doc;
+	printObject(request.body);
+	// generate PDF from object
 	var doc = new pdfkit();
-	
+	doc.pipe(fs.createWriteStream('output.pdf')); //literally no idea if this will work
+	//for (property in request.body) {
+		doc.font('fonts/LiberationSans-Regular.ttf').fontSize(12).text(JSON.stringify(request.body, null, 2));
+	//}
+	response.send(":)");
+	doc.end();
 });
-
+*/
 app.get('/', function(request, response) {
 	response.send('hey');
 });

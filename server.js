655f4704a2accea0a923710c3c88629e1f676b4a
Hayley Cohen <hbc2121@gmail.com>
2017-02-14 15:35:15 -0500

    testing email funcitonality

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.post('/genPDF', function(request, response) {
 	    from: '"HTQR" <htqr2017@gmail.com>', // sender address
 	    to: 'daniellezelin@gmail.com', // list of receivers
 	    subject: 'HTQR Results', // Subject line
-	    text: JSON.stringify(response) + response, // plain text body
+	    text: response, // plain text body
 	   //  attachments: [{
     // 	filename: 'output.pdf',
     // 	path: __dirname + '/output.pdf'

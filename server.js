ee89a6cc6bf80420031a9a9c0d465a617d7db6de
Hayley Cohen <hbc2121@gmail.com>
2017-02-15 17:02:15 -0500

    email edits

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.post('/genPDF', function(request, response) {
 	// setup email data with unicode symbols
 	let mailOptions = { 
 	    from: '"HTQR" <htqr2017@gmail.com>', // sender address
-	    to: data.email, // list of receivers
+	    to: response.email, // list of receivers
 	    subject: 'HTQR Results', // Subject line
 	    text: 'Attached is a PDF of the HTQR Results. Thank You',
 	   	 attachments : [{

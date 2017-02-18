087657c9e29315944b6dfd6f5e1516f092169bc2
Hayley Cohen <hbc2121@gmail.com>
2017-02-18 13:45:24 -0500

    no color

--- server.js
+++ server.js
@@ -88,9 +88,9 @@ app.post('/genPDF', function(request, response) {
 			   .fontSize(12);
 
 		   if (!isNaN(questions[qad]['answer'])){
-		   		doc.text(questions[qad]['question'] + ': ' + words[questions[qad]['answer']].fontcolor("red") + '\n');
+		   		doc.text(questions[qad]['question'] + ': ' + words[questions[qad]['answer']]+ '\n');
 		    } else{
-		   		doc.text(questions[qad]['question'] + ': ' + questions[qad]['answer'].fontcolor("red") + '\n');
+		   		doc.text(questions[qad]['question'] + ': ' + questions[qad]['answer'] + '\n');
 		    }
 
 			if (questions[qad]['dropdown']) {
@@ -98,7 +98,7 @@ app.post('/genPDF', function(request, response) {
 					var dropdown = questions[qad]['dropdown'][info];
 					doc.font('fonts/LiberationSans-Regular.ttf')
 					   .fontSize(12)
-					   .text(tab + dropdown['question']+ ': ' +  dropdown['answer'].fontcolor("red"));
+					   .text(tab + dropdown['question']+ ': ' +  dropdown['answer']);
 				}
 			}
 		}

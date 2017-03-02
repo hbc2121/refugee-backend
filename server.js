de36a6df2811d8a7eda8d286a8c134bf4725180e
Hayley Cohen <hbc2121@gmail.com>
2017-03-02 12:04:45 -0500

    silly ethan

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ var path = require('path');
 //	db = databaseConnection;
 //});
 
-//TODO: why do we need this
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");

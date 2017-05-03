d3de5aff852aa5fe24e56021590cbbe6486ddfba
Hayley Cohen <hbc2121@gmail.com>
2017-05-03 16:20:53 -0400

    testing get patient

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.get('/getPatient', function(request, response){
         lastName: request.body['lastName'],
     };
 
-    response.send("request" + request.body);
+    response.send("I LOVE AMAC" + request.body['firstName']);
 
     // var pat = db.collection('patients').findOne(patientQuery, function(err, patient) {
     // 	if(err){

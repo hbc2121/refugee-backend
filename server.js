f310589e5c46fa8d9e9c7257542a081ebcb4d750
Hayley Cohen <hbc2121@gmail.com>
2017-05-09 13:51:45 -0400

    oid

--- server.js
+++ server.js
@@ -174,7 +174,7 @@ app.post('/addNewPatient', function(request, response) {
 
     var doctorQuery = { userName: request.body['username'] };
     db.collection('patients').findOne(patientQuery, function(err, pat) {
-        db.collection('doctors').updateOne(doctorQuery, { $push: { patients: pat._id.$oid }}, function(err, result) {
+        db.collection('doctors').updateOne(doctorQuery, { $push: { patients: pat._id.valueOf()}}, function(err, result) {
             if (err) {
                 response.send({ "message": "error updating patient list" });
             } else {
@@ -220,7 +220,7 @@ app.get('/getPatient', function(request, response){
     		response.send("error: failed to retrieve patient");	
     	} 
     	if(patient){
-            if(validPatient(patient._id.$oid,request.query.username)){
+            if(validPatient(patient._id.valueOf(),request.query.username)){
                 response.send(patient);
             }else{
                 response.send("error: patient not in doctor list")
@@ -290,7 +290,7 @@ app.post('/addPatientToDoctor', function(request,response){
 
     var pat = db.collection('patients').findOne(patientQuery);
 
-     db.collection('doctors').updateOne(request.body['username'], {$push: {patients: pat._id.$oid}}, function(err, doctor) {
+     db.collection('doctors').updateOne(request.body['username'], {$push: {patients: pat._id.valueOf()}}, function(err, doctor) {
         if (err) {
             reponse.send({ "message": "error: unable to add patient"});
         } else {
@@ -300,14 +300,14 @@ app.post('/addPatientToDoctor', function(request,response){
 
 });
 
-
+//TODO
  function validPatient(id,doctor_name){
  
     var user = db.collection('doctors').findOne({username:doctor_name});
     var patients = user.patients;
     
     console.log(patients);
-    return patients.indexOf(id) > -1;
+    return true;
  }
 
 

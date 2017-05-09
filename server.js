5dfb65c4415c9b701474d5c0aa621d6f8d113768
Hayley Cohen <hbc2121@gmail.com>
2017-05-09 16:35:50 -0400

    testing valid patient

--- server.js
+++ server.js
@@ -320,7 +320,7 @@ app.post('/addPatientToDoctor', function(request,response){
             console.log("ID: " + id + "PATS: " + pats);
             console.log("TYPES " + typeof(id) + " " + typeof(pats[0]));
             console.log("HERE" +pats.includes(id));
-            return (pats.includes(id) > -1);
+            return (pats.includes(id));
         } else {
             return false;
         }

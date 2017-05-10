1b85ba487865f348b48f39622e0808f36eeaa8f1
Hayley Cohen <hbc2121@gmail.com>
2017-05-10 14:55:15 -0400

    patient array

--- server.js
+++ server.js
@@ -301,7 +301,7 @@ app.get('/getPatientsOfDoctor', function(request, response) {
 
             console.log(doctor);
 
-            var patient_array = [];
+            patient_array = [];
 
             console.log("PATIENTS " + patients);
 
